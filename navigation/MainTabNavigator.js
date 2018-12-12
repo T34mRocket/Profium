98be0ebc25a7311a777733b57fac78fe9277b05f
timilil <timi.liljestrom@hotmail.com>
2018-12-12 11:01:19 +0200

    deleted unnecessary screens that are no longer in use

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Platform } from 'react-native'
 import { createStackNavigator } from 'react-navigation'
-import HistoryScreen from '../screens/HistoryScreen'
-import SettingsScreen from '../screens/SettingsScreen'
 import TabBarIcon from '../components/TabBarIcon'
 import HomeScreen from '../screens/HomeScreen'
 import DetailsScreen from '../screens/DetailsScreen'
@@ -12,51 +10,4 @@ const HomeStack = createStackNavigator({
   Details: DetailsScreen
 })
 
-HomeStack.navigationOptions = {
-  /*tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused = {focused}
-      name = {
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),*/
-}
-
-const HistoryStack = createStackNavigator({
-  Links: HistoryScreen,
-})
-
-HistoryStack.navigationOptions = {
-  tabBarLabel: 'Info',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused = {focused}
-      name = {Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
-    />
-  ),
-}
-
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-})
-
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused = {focused}
-      name = {Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
-    />
-  ),
-}
-
-
-export default HomeStack/* createStackNavigator({
-  HomeStack,
-  HistoryStack,
-  SettingsStack,
-})*/
+export default HomeStack
